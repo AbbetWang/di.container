24a86a9da30c0adc900a274ba0a42375024e893c
AbbetWang <AbbetWang@gmail.com>
2024-04-13 00:47:19 +0800

    add: happy starting

---
+++ src/main/java/org/abbet/di/Context.java
@@ -0,0 +1,16 @@
+package org.abbet.di;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context {
+    private Map<Class<?>, Object> components = new HashMap<>();
+
+    public <ComponentType> void bind(Class<ComponentType> componentClass, ComponentType instance) {
+        components.put(componentClass, instance);
+    }
+
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        return (ComponentType) components.get(type);
+    }
+}
--- src/test/java/org/abbet/di/ContainerTest.java
+++ src/test/java/org/abbet/di/ContainerTest.java
@@ -1,20 +1,57 @@
 package org.abbet.di;
 
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
 
 public class ContainerTest {
-    @Nested
-    public class ComponentConstruction{
+
+    interface Component {
 
     }
 
     @Nested
-    public class DependenciesSelection{
+    public class ComponentConstruction {
+        // TODO: instance
+        @Test
+        public void should_bind_type_to_a_specific_instance() {
+            Context context = new Context();
+            Component instance = new Component() {
+            };
+            context.bind(Component.class, instance);
+            assertSame(instance, context.get(Component.class));
+        }
+
+        // TODO: abstract class
+        // TODO: interface
+        @Nested
+        public class ConstructorInjection {
+            //TODO: No args constructor
+            // TODO: with dependencies
+            // TODO: A -> B ->C
+
+        }
+
+        @Nested
+        public class FieldInjection {
+
+        }
+
+        @Nested
+        public class MethodInjection {
+        }
 
     }
 
     @Nested
-    public class LifecycleManagement{
+    public class DependenciesSelection {
+
+    }
+
+    @Nested
+    public class LifecycleManagement {
 
     }
 }
