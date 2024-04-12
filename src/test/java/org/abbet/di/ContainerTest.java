37ebae0dc4e2a92663977cdb81a14e453ca6d707
AbbetWang <AbbetWang@gmail.com>
2024-04-13 00:23:43 +0800

    init:

---
+++ src/test/java/org/abbet/di/ContainerTest.java
@@ -0,0 +1,20 @@
+package org.abbet.di;
+
+import org.junit.jupiter.api.Nested;
+
+public class ContainerTest {
+    @Nested
+    public class ComponentConstruction{
+
+    }
+
+    @Nested
+    public class DependenciesSelection{
+
+    }
+
+    @Nested
+    public class LifecycleManagement{
+
+    }
+}
