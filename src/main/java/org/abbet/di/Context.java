ab77dd441c935459989827ae604b6e85384cba88
AbbetWang <AbbetWang@gmail.com>
2024-04-13 12:13:31 +0800

    refactor:

--- src/main/java/org/abbet/di/Context.java
+++ src/main/java/org/abbet/di/Context.java
@@ -5,23 +5,28 @@ import java.util.Map;
 
 public class Context {
     private Map<Class<?>, Object> components = new HashMap<>();
+    private Map<Class<?>, Class<?>> componentsImplementation = new HashMap<>();
 
     public <ComponentType> void bind(Class<ComponentType> typeClass, ComponentType instance) {
         components.put(typeClass, instance);
     }
 
     public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        return (ComponentType) components.get(type);
+        if (components.containsKey(type)) {
+            return (ComponentType) components.get(type);
+        } else {
+            Class<?> implementation = componentsImplementation.get(type);
+            try {
+                return (ComponentType) implementation.getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
-    public <ComponentType, ComponentImplementation>
+    public <ComponentType, ComponentImplementation extends ComponentType>
     void bind(Class<ComponentType> typeClass, Class<ComponentImplementation> implementation) {
-        try {
-            components.put(typeClass, implementation.newInstance());
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+        componentsImplementation.put(typeClass, implementation);
     }
 }
