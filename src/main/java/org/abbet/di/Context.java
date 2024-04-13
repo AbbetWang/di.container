bb54d463b2e48cac54059a1313f6cc0e30f3eb3a
AbbetWang <AbbetWang@gmail.com>
2024-04-13 12:30:27 +0800

    refactor: di container present

--- src/main/java/org/abbet/di/Context.java
+++ src/main/java/org/abbet/di/Context.java
@@ -1,32 +1,31 @@
 package org.abbet.di;
 
+import jakarta.inject.Provider;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class Context {
-    private Map<Class<?>, Object> components = new HashMap<>();
-    private Map<Class<?>, Class<?>> componentsImplementation = new HashMap<>();
 
-    public <ComponentType> void bind(Class<ComponentType> typeClass, ComponentType instance) {
-        components.put(typeClass, instance);
-    }
+    private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
-    public <ComponentType> ComponentType get(Class<ComponentType> type) {
-        if (components.containsKey(type)) {
-            return (ComponentType) components.get(type);
-        } else {
-            Class<?> implementation = componentsImplementation.get(type);
-            try {
-                return (ComponentType) implementation.getConstructor().newInstance();
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
+    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
+        providers.put(type, (Provider<ComponentType>) () -> instance);
     }
 
     public <ComponentType, ComponentImplementation extends ComponentType>
-    void bind(Class<ComponentType> typeClass, Class<ComponentImplementation> implementation) {
-        componentsImplementation.put(typeClass, implementation);
+    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
+        providers.put(type, (Provider<ComponentType>) () -> {
+            try {
+                return (ComponentType) ((Class<?>) implementation).getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
+
+    public <ComponentType> ComponentType get(Class<ComponentType> type) {
+        return (ComponentType) providers.get(type).get();
+    }
+
 }
