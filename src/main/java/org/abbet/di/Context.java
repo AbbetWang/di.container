5dc79ed79ca6855e07a3d6d0428ed2176a2836a0
AbbetWang <AbbetWang@gmail.com>
2024-04-13 13:07:50 +0800

    update: support constructor inject

--- src/main/java/org/abbet/di/Context.java
+++ src/main/java/org/abbet/di/Context.java
@@ -1,24 +1,45 @@
 package org.abbet.di;
 
+import jakarta.inject.Inject;
 import jakarta.inject.Provider;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
 
 public class Context {
 
     private Map<Class<?>, Provider<?>> providers = new HashMap<>();
 
-    public <ComponentType> void bind(Class<ComponentType> type, ComponentType instance) {
-        providers.put(type, (Provider<ComponentType>) () -> instance);
+    public <Type> void bind(Class<Type> type, Type instance) {
+        providers.put(type, (Provider<Type>) () -> instance);
     }
 
-    public <ComponentType, ComponentImplementation extends ComponentType>
-    void bind(Class<ComponentType> type, Class<ComponentImplementation> implementation) {
-        providers.put(type, (Provider<ComponentType>) () -> {
+    public <Type, Implementation extends Type>
+    void bind(Class<Type> type, Class<Implementation> implementation) {
+        providers.put(type, (Provider<Type>) () -> {
             try {
-                return (ComponentType) ((Class<?>) implementation).getConstructor().newInstance();
+                Constructor<?> injectConstructor = getInjectConstructor(implementation);
+                Object[] dependencies = stream(injectConstructor.getParameters())
+                        .map(p -> get(p.getType()))
+                        .toArray(Object[]::new);
+                return (Type) injectConstructor.newInstance(dependencies);
             } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) throws NoSuchMethodException {
+        Stream<Constructor<?>> injectConstructors = stream(implementation.getConstructors());
+        return (Constructor<Type>) injectConstructors.findFirst().orElseGet(() -> {
+            try {
+                return implementation.getConstructor();
+            } catch (NoSuchMethodException e) {
                 throw new RuntimeException(e);
             }
         });
--- src/test/java/org/abbet/di/ContainerTest.java
+++ src/test/java/org/abbet/di/ContainerTest.java
@@ -1,5 +1,6 @@
 package org.abbet.di;
 
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,15 +9,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ContainerTest {
-
-    interface Component {
-
-    }
-
-    static class ComponentWithDefaultConstructor implements Component {
-        public ComponentWithDefaultConstructor() {
-        }
-    }
 
     Context context;
 
@@ -27,6 +19,7 @@ public class ContainerTest {
 
     @Nested
     public class ComponentConstruction {
+
         // TODO: instance
         @Test
         public void should_bind_type_to_a_specific_instance() {
@@ -40,8 +33,8 @@ public class ContainerTest {
         // TODO: interface
         @Nested
         public class ConstructorInjection {
-            //TODO: No args constructor
 
+            //TODO: No args constructor
             @Test
             public void should_bind_type_to_a_class_with_default_constructor() {
                 context.bind(Component.class, ComponentWithDefaultConstructor.class);
@@ -52,6 +45,17 @@ public class ContainerTest {
             }
 
             // TODO: with dependencies
+
+            @Test
+            public void should_bind_type_to_a_class_with_inject_constructor() {
+                Dependency dependency = new Dependency() {
+                };
+                context.bind(Component.class, ComponentWithInjectConstructor.class);
+                context.bind(Dependency.class, dependency);
+                Component instance = context.get(Component.class);
+                assertNotNull(instance);
+                assertSame(dependency, ((ComponentWithInjectConstructor) instance).getDependency());
+            }
             // TODO: A -> B ->C
 
         }
@@ -59,10 +63,12 @@ public class ContainerTest {
         @Nested
         public class FieldInjection {
 
+
         }
 
         @Nested
         public class MethodInjection {
+
         }
 
     }
@@ -70,10 +76,38 @@ public class ContainerTest {
     @Nested
     public class DependenciesSelection {
 
+
     }
 
     @Nested
     public class LifecycleManagement {
 
+
+    }
+
+    interface Component {
+
+    }
+
+    interface Dependency {
+
+    }
+
+    static class ComponentWithDefaultConstructor implements Component {
+        public ComponentWithDefaultConstructor() {
+        }
+    }
+
+    static class ComponentWithInjectConstructor implements Component {
+        private Dependency dependency;
+
+        @Inject
+        public ComponentWithInjectConstructor(Dependency dependency) {
+            this.dependency = dependency;
+        }
+
+        public Dependency getDependency() {
+            return dependency;
+        }
     }
 }
