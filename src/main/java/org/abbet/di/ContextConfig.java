d9fab7acc83dc82816088fec9b463cf23fb57458
AbbetWang <AbbetWang@gmail.com>
2024-04-20 23:29:55 +0800

    refactor:

--- src/main/java/org/abbet/di/ContextConfig.java
+++ src/main/java/org/abbet/di/ContextConfig.java
@@ -1,12 +1,6 @@
 package org.abbet.di;
 
-import jakarta.inject.Inject;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Parameter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 
@@ -63,50 +57,6 @@ public class ContextConfig {
         T get(Context context);
 
         List<Class<?>> getDependencies();
-    }
-
-    public class ConstructorInjectionProvider<T> implements ComponentProvider<T> {
-        private Constructor<T> injectConstructor;
-
-        public ConstructorInjectionProvider(Class<T> component) {
-            this.injectConstructor = getInjectConstructor(component);
-        }
-
-        private static <Type> Constructor<Type> getInjectConstructor(Class<Type> implementation) {
-            List<Constructor<?>> injectConstructors = stream(implementation.getConstructors())
-                    .filter(c -> c.isAnnotationPresent(Inject.class)).collect(Collectors.toList());
-            if (injectConstructors.size() > 1) throw new IllegalComponentException();
-            return (Constructor<Type>) injectConstructors.stream()
-                    .findFirst().orElseGet(() -> {
-                        try {
-                            return implementation.getConstructor();
-                        } catch (Exception e) {
-                            throw new IllegalComponentException();
-                        }
-                    });
-        }
-
-        @Override
-        public T get(Context context) {
-            try {
-                Object[] dependencies = stream(injectConstructor.getParameters())
-                        .map(p -> {
-                            Class<?> type = p.getType();
-                            return context.get(type).get();
-                        })
-                        .toArray(Object[]::new);
-                return injectConstructor.newInstance(dependencies);
-            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        @Override
-        public List<Class<?>> getDependencies() {
-            return stream(injectConstructor.getParameters())
-                    .map(Parameter::getType)
-                    .collect(Collectors.toList());
-        }
     }
 
 }
